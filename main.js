b6fdbcb8692e856c3db7aa5f1a6f7539165ea786
Kevin Vahdat <kvahdat@nyc.yearup.org>
2018-11-16 12:54:29 -0500

    Finished axios api info

--- main.js
+++ main.js
@@ -1,17 +1,36 @@
-
-
-
-
-
-
-
-
-
-
+class Pokemon {
+  constructor (name, hp, attack, defense, ability) {
+    this.name = name;
+    this.hp = hp;
+    this.attack = attack;
+    this.defense = defense;
+    this.ability = ability;
+  }
+}
 
 
 axios.get("http://fizal.me/pokeapi/api/v2/name/meowth.json")
   .then(function(response) {
-    console.log(response);
-    console.log(response.data.stats[5]);
+    let meowth_hp = response.data.stats[5].base_stat;
+    let meowth_attack = response.data.stats[4].base_stat;
+    let meowth_defense = response.data.stats[3].base_stat;
+    let meowth_ability = response.data.abilities[0].ability;
+
+  })
+
+axios.get("http://fizal.me/pokeapi/api/v2/name/wobbuffet.json")
+  .then(function(response) {
+    let wobb_hp = response.data.stats[5].base_stat;
+    let wobb_attack = response.data.stats[4].base_stat;
+    let wobb_defense = response.data.stats[3].base_stat;
+    let wobb_ability = response.data.abilities[0].ability;
+  })
+
+axios.get("http://fizal.me/pokeapi/api/v2/name/arbok.json")
+  .then(function(response) {
+    let arbok_hp = response.data.stats[5].base_stat;
+    let arbok_attack = response.data.stats[4].base_stat;
+    let arbok_defense = response.data.stats[3].base_stat;
+    let arbok_ability = response.data.abilities[0].ability;
+
   })
